7437078f2fd7b41fc732dc2a297853754aae9673
zhangxiaoxuanaa <zhangxiaoxuan@yahoo.com>
2017-09-10 13:00:21 -0400

    now images are movable! Also added depth stream & depth stream conversion

--- src/snapshot_intel.cpp
+++ src/snapshot_intel.cpp
@@ -4,6 +4,7 @@
 // include opencv
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
 
 #include <cstdio>
 #include <vector>
@@ -104,6 +105,44 @@ bool load_config() {
     return true;
 }
 
+/////////////////////////////////////////////////////////////////////////////
+// Called every frame gets the data from streams and displays them using OpenCV.
+/////////////////////////////////////////////////////////////////////////////
+/**
+ bool display_next_frame( )
+{
+
+       _depth_intrin       = _rs_camera.get_stream_intrinsics( rs::stream::depth );
+       _color_intrin       = _rs_camera.get_stream_intrinsics( rs::stream::color );
+
+
+       // Create depth image
+       cv::Mat depth16( _depth_intrin.height,
+                                  _depth_intrin.width,
+                                  CV_16U,
+                                  (uchar *)_rs_camera.get_frame_data( rs::stream::depth ) );
+
+       // Create color image
+       cv::Mat rgb( _color_intrin.height,
+                            _color_intrin.width,
+                            CV_8UC3,
+                            (uchar *)_rs_camera.get_frame_data( rs::stream::color ) );
+
+       // < 800
+       cv::Mat depth8u = depth16;
+       depth8u.convertTo( depth8u, CV_8UC1, 255.0/1000 );
+
+       imshow( WINDOW_DEPTH, depth8u );
+       cvWaitKey( 1 );
+
+       cv::cvtColor( rgb, rgb, cv::COLOR_BGR2RGB );
+       imshow( WINDOW_RGB, rgb );
+       cvWaitKey( 1 );
+
+       return true;
+}
+**/
+
 
 int main () try {
 	std::cerr << "Loading configuration file...";
@@ -128,13 +167,13 @@ int main () try {
 
 	    // Configure all streams to run at 640*480 at 60 frames per second
 	    if(depth_enable){
-	        dev->enable_stream(rs::stream::depth, 640, 480, rs::format::z16, 60);
+	        dev->enable_stream(rs::stream::depth, 640, 480, rs::format::z16, 30);
 	    }
 	    if(rgb_enable){
-	        dev->enable_stream(rs::stream::color, 640, 480, rs::format::rgb8, 60);
+	        dev->enable_stream(rs::stream::color, 640, 480, rs::format::rgb8, 30);
 	    }
 	    if(ir_enable){
-	        dev->enable_stream(rs::stream::infrared, 640, 480, rs::format::y8, 60);
+	        dev->enable_stream(rs::stream::infrared, 640, 480, rs::format::y8, 30);
 	    }
 	    if(!depth_enable && !rgb_enable && !ir_enable){
 	    	printf("No stream enabled, program exits automatically.\n");
@@ -147,16 +186,29 @@ int main () try {
 	       dev->wait_for_frames();
 	    }
 
-	    // Display in a GUI
-	    cv::namedWindow("Display Image");
-
 	    for(int i = 0; i < capture_num; i++){
-	       cv::Mat color(cv::Size(640, 480), CV_8UC3, (void*)dev->get_frame_data(rs::stream::color), cv::Mat::AUTO_STEP);
-	       cv::imshow("Display Image", color);
+	       if( dev->is_streaming() )
+	    	   dev->wait_for_frames();
+
+	       if(depth_enable && depth_plot){
+	    	   cv::Mat depth(cv::Size(640, 480), CV_16UC1, (void*)dev->get_frame_data(rs::stream::depth), cv::Mat::AUTO_STEP);
+	    	   // Convert 16bit to 8 bit
+	           depth.convertTo( depth, CV_8UC1, 255.0/1000 );
+	    	   cv::imshow("Depth", depth);
+	       }
+	       if(rgb_enable && rgb_plot){
+	    	   cv::Mat color(cv::Size(640, 480), CV_8UC3, (void*)dev->get_frame_data(rs::stream::color), cv::Mat::AUTO_STEP);
+	    	   cv::imshow("RGB", color);
+	       }
+	       if(ir_enable && ir_plot){
+	    	   cv::Mat ir(cv::Size(640, 480), CV_8UC1, (void*)dev->get_frame_data(rs::stream::infrared), cv::Mat::AUTO_STEP);
+	    	   cv::imshow("IR", ir);
+	       }
 	       int key = cv::waitKey(200);
 	       switch(key) {
 	       	case 27:
-	        	break;
+	       		printf("Quitting program\n");
+	    	    return EXIT_SUCCESS;
 	       }
 	    }
 	    return EXIT_SUCCESS;
