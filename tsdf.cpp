4b0a07dc88c6deb7d0cca22f9b6b6ea1a6be419d
motion <motion@tosh.egr.duke.edu>
2017-10-14 19:03:35 -0400

    finished point counting, now display interior of a scene

--- tsdf.cpp
+++ tsdf.cpp
@@ -3,10 +3,8 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/visualization/cloud_viewer.h>
 
-#include <cstdio>
-#include <string>
-#include <iostream>
-#include <fstream>
+using namespace pcl;
+using namespace std;
 
 int main (int argc, char * argv[])
 {
@@ -29,25 +27,33 @@ int main (int argc, char * argv[])
   std::cout.flush();
   float tsdf_value = 0;
   pcl::PointCloud<pcl::PointXYZ> cloud;
-
+  long count1 = 0;
+  long count2 = 0;
+  long count3 = 0;
   for (int i = 0; i < 512; i++) {
     for (int j = 0; j < 512; j++) {
       for (int k = 0; k < 512; k++) {
         if(fread((void*)(&tsdf_value), sizeof(tsdf_value), 1, fp)) {
-          if (tsdf_value > 0) {
+          if (tsdf_value < 0) {
             cloud.push_back(pcl::PointXYZ(i, j, k));
+            count3++;
+          }
+          if(tsdf_value==0){
+              count2++;
           }
         }
+          if(tsdf_value>0){
+              count1++;
+          }
       }
     }
   }
+  std::cout << count1 << " " << count2 << " " << count3 << std::endl;
   pcl::io::savePCDFileASCII(tsdfDirectory+"/tsdfcloud.pcd",cloud);
   std::cout << "TSDF point cloud generated." << std::endl;
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_new (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::io::loadPCDFile ("./tsdfcloud.pcd", *cloud_new);
   pcl::visualization::CloudViewer viewer("Cloud Viewer");
-  //blocks until the cloud is actually rendered
-  viewer.showCloud(cloud_new);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr ptrCloud(&cloud);
+  viewer.showCloud(ptrCloud);
   while (!viewer.wasStopped ())
   {
 
